63a73f4cc061075b6ea51589e23fe16295a98b61
Daniel-20211697 <103225266+Daniel-20211697@users.noreply.github.com>
2023-11-22 18:56:11 -0400

    Eliminacion de código obsoleto

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,5 +1,4 @@
 
-
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
@@ -8,38 +7,6 @@ import java.sql.SQLException;
 
 public class Conexion {
     public static Connection conexion;
-    /*
-    public static Connection getConnection(){
-        
-        try{
-            Runtime.getRuntime().addShutdownHook(new getClose);
-            if(conexion== null){
-                Class.forName("com.mysql.cj.jdbc.Driver");
-            conexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/tarea4","root","");
-                
-            }
-          
-            return conexion;
-        }catch(ClassNotFoundException | SQLException e){
-            throw new RuntimeException("Conexion Establecida",e);
-            
-        }
-    }
-    static class getClose extends Thread(){
-       
-        @Override
-        public void run(){
-            try{
-                
-                Connection conn= Conexion.getConnection();
-                conn.close();
-            }catch(SQLException ex){
-                throw new RuntimeException(ex)
-                
-            }
-        }
-
-    }
-*/
+    
 }
 
