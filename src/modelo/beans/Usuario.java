562011c29747b2a2a455f5920bf7d736d44a330c
Daniel-20211697 <103225266+Daniel-20211697@users.noreply.github.com>
2023-11-24 12:25:50 -0400

    Update Usuario.java

--- src/modelo/beans/Usuario.java
+++ src/modelo/beans/Usuario.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo.beans;
 
 public class Usuario {
@@ -81,9 +77,6 @@ public class Usuario {
     public void setCorreo(String correo) {
         this.correo = correo;
     }
-    
-    
-    
-           
+       
     
 }
