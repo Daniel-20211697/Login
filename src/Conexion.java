10d0326e35e3184b52ed904621a23859577bc0f9
Daniel-20211697 <103225266+Daniel-20211697@users.noreply.github.com>
2023-11-24 13:08:16 -0400

    Update Conexion.java

--- src/Conexion.java
+++ src/Conexion.java
@@ -8,38 +8,6 @@ import java.sql.SQLException;
 
 public class Conexion {
     public static Connection conexion;
-    /*
-    public static Connection getConnection(){
-        
-        try{
-            Runtime.getRuntime().addShutdownHook(new getClose);
-            if(conexion== null){
-                Class.forName("com.mysql.cj.jdbc.Driver");
-            conexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/tarea4","root","");
-                
-            }
-          
-            return conexion;
-        }catch(ClassNotFoundException | SQLException e){
-            throw new RuntimeException("Conexion Establecida",e);
-            
-        }
-    }
-    static class getClose extends Thread(){
-       
-        @Override
-        public void run(){
-            try{
-                
-                Connection conn= Conexion.getConnection();
-                conn.close();
-            }catch(SQLException ex){
-                throw new RuntimeException(ex)
-                
-            }
-        }
-
-    }
-*/
+    
 }
 
